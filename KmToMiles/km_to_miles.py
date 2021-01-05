163f0da7ed559ce3ad658da5b29b7e33b9aae605
shivraj-thecoder <76987533+shivraj-thecoder@users.noreply.github.com>
2021-01-05 11:57:06 +0530

    Create km_to_miles.py
    Km to Miles
    
    Co-Authored-By: Ajinkya Patil <san.avpatil@gmail.com>

---
+++ KmToMiles/km_to_miles.py
@@ -0,0 +1,11 @@
+# distance_km= 564.5
+# 1 km = 0.621371 miles
+# create programme to convert distance in kilometersto miles?
+
+SPKM= 0.621371
+
+Distance= input("Enter distance have to convert :")
+
+Dist = SPKM * Distance
+
+print("Converted_Distance = ",Dist)
