971601239635b6f5bf42bb8ad07eccb0f4882455
shivraj-thecoder <76987533+shivraj-thecoder@users.noreply.github.com>
2021-01-11 10:32:38 +0530

    Update km_to_miles.py

--- KmToMiles/km_to_miles.py
+++ KmToMiles/km_to_miles.py
@@ -1,5 +1,3 @@
-# create programme to convert distance in kilometers to miles
-# distance_km= 564.5
 # 1 km = 0.621371 miles
 
 # constant value for conversion
@@ -7,6 +5,28 @@ km_mile_const = 0.621371
 # Get distance input from user for conversion
 dist_km= input("Enter Distance In KM : ")
 # Convert to miles
-dist_miles = km_mile_const * int(dist_km)
+dist_miles = km_mile_const * float(dist_km)
 # Print Calculated Distance
 print("Converted Distance In Miles : ", dist_miles)
+
+
+
+
+
+
+
+
+
+
+
+# distance_km= 564.5
+# 1 km = 0.621371 miles
+# create programme to convert distance in kilometersto miles?
+
+SPKM= 0.621371
+
+Distance= input("Enter distance have to convert :")
+
+Dist = SPKM * float(Distance)
+
+print("Converted_Distance = ",Dist)
