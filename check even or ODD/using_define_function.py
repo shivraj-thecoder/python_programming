e02c6dd7202856ca007b740ad80a4d74847b4230
Ajinkya Patil <san.avpatil@gmail.com>
2021-01-20 18:17:17 +0530

    Update using_define_function.py

--- check even or ODD/using_define_function.py
+++ check even or ODD/using_define_function.py
@@ -1,11 +1,8 @@
 def check_even(value_x):
-    if remainder of int x is 2 and quotient equalto 0 :
+    if x%2 == 0 :
         return True
-        
-    else return false 
-
-
-
+    else:
+        return false 
 
 x=int(input("enter a number :"))
 x = check_even(x)
