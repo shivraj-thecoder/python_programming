303a7ba8cfe651a62b7192795bad80c38054adf1
shivraj-thecoder <76987533+shivraj-thecoder@users.noreply.github.com>
2021-01-20 16:09:35 +0530

    check correction
    Co-Authored-By: Ajinkya Patil <san.avpatil@gmail.com>

---
+++ check even or ODD/using_define_function.py
@@ -0,0 +1,12 @@
+def check_even(value_x):
+    if remainder of int x is 2 and quotient equalto 0 :
+        return True
+        
+    else return false 
+
+
+
+
+x=int(input("enter a number :"))
+x = check_even(x)
+ print(x)    
